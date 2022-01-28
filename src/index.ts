7a9279bf3ad2b090f9d8ddcd2b3ac0a54d515652
Vasiliy Alfertev <vasiliy.alfertev@corp.codeinside.ru>
2022-01-28 16:52:41 +0300

    Initial commit.

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { el, fr, ref, Component, createRef, renderTemplate } from 'rwrtw';
+
+let counter = 0
+const hello = createRef<Element>()
+const even = createRef<Component>()
+const odd = createRef<Component>()
+
+renderTemplate({ parent: document.body },
+    el('div')(
+        el('h1')(
+            'It Works!'
+        ),
+        ref(hello, el('p', { class: 'paragraph' })(
+            'Hello world!'
+        )),
+        el('button', null, {
+            click: () => {
+                hello.current.textContent = `Hello world ${counter++} times!`
+                if (counter % 2 === 0) {
+                    even.current.show()
+                    odd.current.hide()
+                } else {
+                    even.current.hide()
+                    odd.current.show()
+                }
+            }
+        })('Increment'),
+        el('div')(
+            ref(even, fr(el('p')("Even!"))),
+            ref(odd, fr(el('p')("Odd!"))),
+        )
+    )
+)
+
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.ts',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'RWRTW test',
+        }),
+    ],
+    devtool: 'source-map',
+    devServer: {
+        static: './dist'
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            include: path.resolve(__dirname, 'src'),
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    }
+};
