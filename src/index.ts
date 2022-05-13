038622aad94c792a65136671225eee9a2215e988
Vasiliy Alfertev <vasiliy.alfertev@corp.codeinside.ru>
2022-05-13 23:08:02 +0300

    Use 0.0.4.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-import { el, ref, createRef, renderTo, hidable, Hidable, fr } from 'rwrtw'
+import { el, ref, createRef, hidable, Hidable, Placeholder } from 'rwrtw'
 
 let counter = 0
 const hello = createRef<HTMLElement>()
 const even = createRef<Hidable>()
 const odd = createRef<Hidable>()
 
-const app = renderTo(
-    { parent: document.body },
+const root = new Placeholder({ parent: document.body })
+
+root.setContent(
     el('div')(
         el('h1')('It Works!'),
         ref(hello, el('p', { class: 'paragraph' })('Hello world!')),
         el('button', null, {
             click: () => {
-                hello.current.textContent = `Hello world ${counter++} times!`
+                hello.current.textContent = `Hello world ${++counter} times!`
                 if (counter % 2 === 0) {
                     even.current.show()
                     odd.current.hide()
@@ -22,8 +23,6 @@ const app = renderTo(
                 }
             },
         })('Increment'),
-        el('div')(ref(even, hidable(fr(el('p')('Even!')))), ref(odd, hidable(fr(el('p')('Odd!')))))
+        el('div')(ref(even, hidable(el('p')('Even!'))), ref(odd, hidable(el('p')('Odd!'))))
     )
 )
-
-app.mount()
