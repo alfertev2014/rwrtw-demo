527eed3d4e8d8373ca96eded1564db4054fa29c8
Василий <valfertev@rtk-element.ru>
2023-10-16 18:42:41 +0300

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import { cmpnt, createRootPlaceholder, on } from 'rwrtw'
 import { button, div, h1, p } from 'rwrtw/lib/abbrev'
-import { ifElse, IfElse } from 'rwrtw/lib/mvvm'
+import { ifElse, IfElse } from 'rwrtw'
 
-let counter = 0
-let hello: HTMLElement
-let evenOdd: IfElse
+const app = cmpnt(() => {
+    let counter = 0
+    let hello: HTMLElement
+    let evenOdd: IfElse
+    
+    const handleClick = () => {
+        hello.textContent = `Hello world ${++counter} times!`
+        evenOdd.condition = counter % 2 === 0
+    }
 
-const handleClick = () => {
-    hello.textContent = `Hello world ${++counter} times!`
-    evenOdd.condition = counter % 2 === 0
-}
-
-const app = cmpnt(() =>
-    div()(
+    return div()(
         h1()('It Works!'),
         p({ class: 'paragraph' }, (_) => {
             hello = _
@@ -32,7 +32,7 @@ const app = cmpnt(() =>
             ),
         )
     )
-)
+})
 
 const root = createRootPlaceholder(document.body)
 
