df3ff19c360c301f7b3131a76de2eacae2619161
Василий <valfertev@rtk-element.ru>
2023-10-16 15:10:56 +0300

    Use ifElse.

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,39 @@
-import { createRef, createRootPlaceholder } from 'rwrtw'
+import { cmpnt, createRootPlaceholder, on } from 'rwrtw'
 import { button, div, h1, p } from 'rwrtw/lib/abbrev'
-import { Hidable, hidable } from 'rwrtw/lib/components'
+import { ifElse, IfElse } from 'rwrtw/lib/mvvm'
 
 let counter = 0
-const hello = createRef<HTMLElement>()
-const even = createRef<Hidable>()
-const odd = createRef<Hidable>()
+let hello: HTMLElement
+let evenOdd: IfElse
+
+const handleClick = () => {
+    hello.textContent = `Hello world ${++counter} times!`
+    evenOdd.condition = counter % 2 === 0
+}
+
+const app = cmpnt(() =>
+    div()(
+        h1()('It Works!'),
+        p({ class: 'paragraph' }, (_) => {
+            hello = _
+        })('Hello world!'),
+        button(
+            null,
+            on({
+                click: handleClick,
+            })
+        )('Increment'),
+        div()(
+            ifElse(
+                true,
+                () => p()('Even!'),
+                () => p()('Odd!'),
+                (_) => (evenOdd = _)
+            ),
+        )
+    )
+)
 
 const root = createRootPlaceholder(document.body)
 
-root.setContent(
-    div()(
-        h1()('It Works!'),
-        hello.as(p({ class: 'paragraph' })('Hello world!')),
-        button(null, {
-            click: () => {
-                hello.current.textContent = `Hello world ${++counter} times!`
-                if (counter % 2 === 0) {
-                    even.current.show()
-                    odd.current.hide()
-                } else {
-                    even.current.hide()
-                    odd.current.show()
-                }
-            },
-        })('Increment'),
-        div()(even.as(hidable(p()('Even!'))), odd.as(hidable(p()('Odd!'))))
-    )
-)
+root.setContent(app())
